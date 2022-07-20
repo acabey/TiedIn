143834eee485207361bfdf93bc985cd6b4c658b5
Andrew Cabey <me@acabey.xyz>
2022-07-19 20:55:29 -0400

    Configured Apollo GraphQL and OpenBeta endpoint

---
+++ app/src/main/java/edu/neu/tiedin/MainActivity.java
@@ -0,0 +1,27 @@
+package edu.neu.tiedin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import graphql.ExecutionResult;
+import graphql.GraphQL;
+import graphql.schema.GraphQLSchema;
+import graphql.schema.StaticDataFetcher;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.SchemaParser;
+import graphql.schema.idl.TypeDefinitionRegistry;
+
+import static graphql.schema.idl.RuntimeWiring.newRuntimeWiring;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}
